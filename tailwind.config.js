289cd364b03a2b96242b515fec5f9be5a6ea39f9
陈钊 <wb.chenzhao01@mesg.corp.netease.com>
2023-11-22 17:58:46 +0800

    feat: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
