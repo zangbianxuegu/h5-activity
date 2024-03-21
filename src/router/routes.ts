ba620135c07f4dad67e7e1c194381819c55d82f1
陈钊 <wb.chenzhao01@mesg.corp.netease.com>
2024-03-21 10:28:09 +0800

    test: add sign in 1 page

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,45 +7,45 @@ export const routes: RouteRecordRaw[] = [
     // component: () => import('@/views/Home'),
     redirect: '/sign-mayday-2024',
   },
-  // {
-  //   path: '/holiday',
-  //   name: 'Holiday',
-  //   component: () => import('@/views/Holiday'),
-  //   meta: {
-  //     title: '假日打卡',
-  //     module: 'activity_sign_in_1',
-  //   },
-  // },
-  // {
-  //   path: '/signin',
-  //   name: 'Signin',
-  //   redirect: '/signin/winter',
-  //   component: () => import('@/views/Signin'),
-  //   meta: {
-  //     title: '签到活动',
-  //     module: 'signin',
-  //   },
-  //   children: [
-  //     {
-  //       path: 'winter',
-  //       name: 'Winter',
-  //       component: () => import('@/views/Signin/Winter'),
-  //       meta: {
-  //         title: '冬季签到',
-  //         module: 'activity_sign_in_2',
-  //       },
-  //     },
-  //     {
-  //       path: 'summer',
-  //       name: 'Summer',
-  //       component: () => import('@/views/Signin/Summer'),
-  //       meta: {
-  //         title: '暑假签到',
-  //         module: 'activity_sign_in_3',
-  //       },
-  //     },
-  //   ],
-  // },
+  {
+    path: '/holiday',
+    name: 'Holiday',
+    component: () => import('@/views/Holiday'),
+    meta: {
+      title: '假日打卡',
+      module: 'activity_sign_in_1',
+    },
+  },
+  {
+    path: '/signin',
+    name: 'Signin',
+    redirect: '/signin/winter',
+    component: () => import('@/views/Signin'),
+    meta: {
+      title: '签到活动',
+      module: 'signin',
+    },
+    children: [
+      {
+        path: 'winter',
+        name: 'Winter',
+        component: () => import('@/views/Signin/Winter'),
+        meta: {
+          title: '冬季签到',
+          module: 'activity_sign_in_2',
+        },
+      },
+      {
+        path: 'summer',
+        name: 'Summer',
+        component: () => import('@/views/Signin/Summer'),
+        meta: {
+          title: '暑假签到',
+          module: 'activity_sign_in_3',
+        },
+      },
+    ],
+  },
   {
     path: '/sign-mayday-2024',
     name: 'SignMayday2024',
