f153cafeb505d1258346b93a78c08722d8b67f00
陈钊 <wb.chenzhao01@mesg.corp.netease.com>
2024-04-18 11:55:33 +0800

    fix: #48413 【活动中心】活动中心其他活动关闭后，剩下小光快报时，进入活动中心会有概率出现不主动选择小光快报的情况

--- src/App.vue
+++ src/App.vue
@@ -45,94 +45,11 @@
 </template>
 
 <script setup lang="ts">
-import dayjs from 'dayjs'
 import { showToast } from 'vant'
 import Menu from '@/components/Menu'
-import { type MenuItem, type Activity, type ActivityTime } from '@/types'
-import {
-  getPlayerMissionData,
-  getUserInfo,
-  getJinglingToken,
-} from '@/utils/request'
+import { getUserInfo, getJinglingToken } from '@/utils/request'
 import { Session } from '@/utils/storage'
 import { useBaseStore } from '@/stores/base'
-import { useMenuStore } from '@/stores/menu'
-import { useActivityStore } from '@/stores/activity'
-import { useRoute, useRouter } from 'vue-router'
-
-// 菜单初始值
-const initMenuItems: MenuItem[] = [
-  {
-    label: '巢落大地筑梦共时',
-    value: 'activity_season22_start',
-    routeName: 'Season22Start',
-    isNew: false,
-    isClaimedReward: true,
-    children: [],
-  },
-  {
-    label: '田月桑时春风雀跃',
-    value: 'activity_sign_mayday_2024',
-    routeName: 'SignMayday2024',
-    isNew: false,
-    isClaimedReward: true,
-    children: [],
-  },
-  {
-    label: '大耳狗茶话会',
-    value: 'activity_sanrio_2024',
-    routeName: 'Sanrio2024',
-    isNew: false,
-    isClaimedReward: true,
-    children: [],
-  },
-  {
-    label: '予光千缕碧浪万顷',
-    value: 'activity_nature_2024',
-    routeName: 'Nature2024',
-    isNew: false,
-    isClaimedReward: true,
-    children: [],
-  },
-  {
-    label: '假日打卡',
-    value: 'activity_sign_in_1',
-    routeName: 'Holiday',
-    isNew: false,
-    isClaimedReward: true,
-    children: [],
-  },
-  {
-    label: '签到活动',
-    value: 'signin',
-    routeName: 'Signin',
-    isNew: false,
-    isClaimedReward: true,
-    children: [
-      {
-        label: '冬季签到',
-        value: 'activity_sign_in_2',
-        routeName: 'Winter',
-        isNew: false,
-        isClaimedReward: true,
-      },
-      {
-        label: '暑假签到',
-        value: 'activity_sign_in_3',
-        routeName: 'Summer',
-        isNew: false,
-        isClaimedReward: true,
-      },
-    ],
-  },
-  {
-    label: '小光快报',
-    value: 'activity_center_notice',
-    routeName: 'Bulletin',
-    isNew: false,
-    isClaimedReward: true,
-  },
-]
 
 const localUrl = 'https://10.227.198.175:5173'
 const gameUrl =
@@ -143,39 +60,17 @@ const isLocal = ref(window.location.href.includes(localUrl))
 const isGame = ref(window.location.href.includes(gameUrl))
 const isGameDev = ref(window.location.href.includes(gameDevUrl))
 
-const route = useRoute()
-const router = useRouter()
-
 const token = Session.get('jinglingToken')
 
 // 基本信息
 const baseStore = useBaseStore()
 const { updateBaseInfoItems } = baseStore
 
-// 菜单数据
-const menuStore = useMenuStore()
-const { updateMenuData } = menuStore
-
-// 活动信息
-const activityStore = useActivityStore()
-const { updateActivityTime } = activityStore
-
-// 接口数据类型
-interface ActivityData {
-  start_time: number
-  end_time: number
-  is_new: number
-  is_claimed_reward: number
-  active: number
-}
-type Activities = Record<string, ActivityData>
-
 onMounted(() => {
   try {
-    getAllEvents()
     getBaseInfo()
   } catch (error) {
-    // console.error(error)
+    console.error(error)
   }
 })
 
@@ -228,148 +123,6 @@ function handleToSprite(): void {
         showToast(error.message)
       })
   }
-}
-
-// 抽取有效的活动信息
-function extractActiveEvents(activitiesResponse: Activities): Activity[] {
-  return Object.entries(activitiesResponse)
-    .reduce<Activity[]>((activeEvents, [activityName, activityInfo]) => {
-      if (activityInfo.active === 1) {
-        activeEvents.push({
-          activity: activityName,
-          startTime: activityInfo.start_time,
-          endTime: activityInfo.end_time,
-          isNew: activityInfo.is_new === 1,
-          isClaimedReward:
-            activityName === 'activity_center_notice'
-              ? true
-              : activityInfo.is_claimed_reward === 1,
-        })
-      }
-      return activeEvents
-    }, [])
-    .sort((a, b) => {
-      // 如果是小光快报，让它排在最后
-      if (a.activity === 'activity_center_notice') return 1
-      if (b.activity === 'activity_center_notice') return -1
-      return b.startTime - a.startTime
-    })
-}
-
-// 生成菜单数据
-function generateMenuData(
-  menuItems: MenuItem[],
-  activeEvents: Activity[],
-): MenuItem[] {
-  const activeEventsMap = new Map(
-    activeEvents.map((event) => [event.activity, event]),
-  )
-  const filteredMenuItems = menuItems.reduce<MenuItem[]>(
-    (activeMenu, menuItem) => {
-      const event = activeEventsMap.get(menuItem.value)
-      const updatedMenuItem = { ...menuItem }
-      if (event) {
-        updatedMenuItem.isNew = event.isNew
-        updatedMenuItem.isClaimedReward = event.isClaimedReward
-        activeMenu.push(updatedMenuItem)
-      } else if (menuItem.children && menuItem.children.length > 0) {
-        const activeChildren = generateMenuData(menuItem.children, activeEvents)
-        if (activeChildren.length) {
-          updatedMenuItem.children = activeChildren
-          activeMenu.push(updatedMenuItem)
-        }
-      }
-      return activeMenu
-    },
-    [],
-  )
-
-  // 对菜单进行排序，确保顺序与 activeEvents 一致
-  function sortMenuItemsByActiveEventsOrder(menuItems: MenuItem[]): MenuItem[] {
-    return menuItems.sort((a, b) => {
-      const indexA = activeEvents.findIndex(
-        (event) => event.activity === a.value,
-      )
-      const indexB = activeEvents.findIndex(
-        (event) => event.activity === b.value,
-      )
-      return indexA - indexB
-    })
-  }
-  const sortedMenuItems = sortMenuItemsByActiveEventsOrder(filteredMenuItems)
-  sortedMenuItems.forEach((menuItem) => {
-    if (menuItem.children && menuItem.children.length > 0) {
-      menuItem.children = sortMenuItemsByActiveEventsOrder(menuItem.children) // 对子菜单也应用排序
-    }
-  })
-
-  return sortedMenuItems
-}
-
-// 获取所有活动信息
-function getAllEvents(): void {
-  getPlayerMissionData({})
-    .then((res) => {
-      const activeEvents = extractActiveEvents(res.data.event_data)
-      const newMenuData = generateMenuData(initMenuItems, activeEvents)
-      console.log('newMenuData: ', newMenuData)
-      if (!newMenuData || newMenuData.length === 0) {
-        showToast('网络连接异常，请稍后重试')
-        return
-      }
-      // 跳转到第一个活动页面：
-      // - 进入首页 /
-      // - 活动未开启
-
-      // fix: 有可能 route.path === '/'，而 route.name === undefined
-      // 在修改活动页面比如Nature2024.vue，然后刷新页面时发生（MuMu模拟器）
-      // 注意 route 作为对象不能实时的反应状态信息
-      // console.log('route: ', route)
-      // console.log('route.name: ', route.name)
-      // console.log('route.path: ', route.path)
-      if (
-        newMenuData.length > 0 &&
-        ((route.path === '/' && route.name === 'Home') ||
-          (route.name !== undefined &&
-            !newMenuData.find((item) => item.routeName === route.name)))
-      ) {
-        // console.log('开始 router replace')
-        void router.replace({ name: newMenuData[0].routeName })
-      }
-
-      // 活动时间
-      const newActivityTime = activeEvents
-        .filter((item) => item.startTime && item.endTime)
-        .reduce<ActivityTime>((acc, cur) => {
-          acc[cur.activity] = formatDate(cur.startTime, cur.endTime)
-          return acc
-        }, {})
-
-      // 更新 store 存储
-      if (res.data?.current_time) {
-        updateBaseInfoItems({ currentTime: res.data.current_time })
-      }
-      updateActivityTime(newActivityTime)
-      updateMenuData(newMenuData)
-
-      // fix: 首次请求完菜单信息之后更新红点状态，路由拦截时数据还是初始数据
-      const module = route.meta.module
-
-      if (module && typeof module === 'string') {
-        menuStore.updatedMenuDataByRoute(module)
-      }
-    })
-    .catch((error) => {
-      console.log('app.vue', error)
-      showToast(error.message)
-    })
-}
-
-// 格式化日期
-function formatDate(startTime: number, endTime: number): string {
-  const start = dayjs.unix(startTime)
-  const end = dayjs.unix(endTime)
-  return `${start.format('M.D')}-${end.format('M.D')}`
 }
 </script>
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,26 @@
 import { showToast } from 'vant'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import {
+  type RouteLocationNormalized,
+  createRouter,
+  createWebHashHistory,
+} from 'vue-router'
 import { routes } from './routes'
-import { webViewStatistics } from '@/utils/request'
+import { useBaseStore } from '@/stores/base'
 import { useMenuStore } from '@/stores/menu'
+import { useActivityStore } from '@/stores/activity'
+import dayjs from 'dayjs'
+import { type MenuItem, type Activity, type ActivityTime } from '@/types'
+import { getPlayerMissionData, webViewStatistics } from '@/utils/request'
+
+// 接口数据类型
+interface ActivityData {
+  start_time: number
+  end_time: number
+  is_new: number
+  is_claimed_reward: number
+  active: number
+}
+type Activities = Record<string, ActivityData>
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -10,7 +28,7 @@ const router = createRouter({
 })
 
 router.beforeEach((to, _, next) => {
-  console.log('页面路由 to: ', to)
+  console.log('beforeEach to: ', to)
   const title = to?.meta?.title
   const module = to?.meta?.module
   if (title) {
@@ -40,5 +58,239 @@ router.beforeEach((to, _, next) => {
     next()
   }
 })
+
+router.afterEach((to, from) => {
+  console.log('afterEach to, from: ', to, from)
+  const menuStore = useMenuStore()
+  const menuData = computed(() => menuStore.menuData)
+  console.log('menuData.value: ', menuData.value)
+  if (menuData.value.length > 0) {
+    if (
+      to.path === '/' ||
+      !menuData.value.find((item) => item.routeName === to.name)
+    ) {
+      console.log('开始 router replace')
+      void router.replace({ name: menuData.value[0].routeName })
+    }
+  } else {
+    getAllEvents(to)
+  }
+})
+
+// 菜单初始值
+const initMenuItems: MenuItem[] = [
+  {
+    label: '巢落大地筑梦共时',
+    value: 'activity_season22_start',
+    routeName: 'Season22Start',
+    isNew: false,
+    isClaimedReward: true,
+    children: [],
+  },
+  {
+    label: '田月桑时春风雀跃',
+    value: 'activity_sign_mayday_2024',
+    routeName: 'SignMayday2024',
+    isNew: false,
+    isClaimedReward: true,
+    children: [],
+  },
+  {
+    label: '大耳狗茶话会',
+    value: 'activity_sanrio_2024',
+    routeName: 'Sanrio2024',
+    isNew: false,
+    isClaimedReward: true,
+    children: [],
+  },
+  {
+    label: '予光千缕碧浪万顷',
+    value: 'activity_nature_2024',
+    routeName: 'Nature2024',
+    isNew: false,
+    isClaimedReward: true,
+    children: [],
+  },
+  {
+    label: '假日打卡',
+    value: 'activity_sign_in_1',
+    routeName: 'Holiday',
+    isNew: false,
+    isClaimedReward: true,
+    children: [],
+  },
+  {
+    label: '签到活动',
+    value: 'signin',
+    routeName: 'Signin',
+    isNew: false,
+    isClaimedReward: true,
+    children: [
+      {
+        label: '冬季签到',
+        value: 'activity_sign_in_2',
+        routeName: 'Winter',
+        isNew: false,
+        isClaimedReward: true,
+      },
+      {
+        label: '暑假签到',
+        value: 'activity_sign_in_3',
+        routeName: 'Summer',
+        isNew: false,
+        isClaimedReward: true,
+      },
+    ],
+  },
+  {
+    label: '小光快报',
+    value: 'activity_center_notice',
+    routeName: 'Bulletin',
+    isNew: false,
+    isClaimedReward: true,
+  },
+]
+
+// 抽取有效的活动信息
+function extractActiveEvents(activitiesResponse: Activities): Activity[] {
+  return Object.entries(activitiesResponse)
+    .reduce<Activity[]>((activeEvents, [activityName, activityInfo]) => {
+      if (activityInfo.active === 1) {
+        activeEvents.push({
+          activity: activityName,
+          startTime: activityInfo.start_time,
+          endTime: activityInfo.end_time,
+          isNew: activityInfo.is_new === 1,
+          isClaimedReward:
+            activityName === 'activity_center_notice'
+              ? true
+              : activityInfo.is_claimed_reward === 1,
+        })
+      }
+      return activeEvents
+    }, [])
+    .sort((a, b) => {
+      // 如果是小光快报，让它排在最后
+      if (a.activity === 'activity_center_notice') return 1
+      if (b.activity === 'activity_center_notice') return -1
+      return b.startTime - a.startTime
+    })
+}
+
+// 生成菜单数据
+function generateMenuData(
+  menuItems: MenuItem[],
+  activeEvents: Activity[],
+): MenuItem[] {
+  const activeEventsMap = new Map(
+    activeEvents.map((event) => [event.activity, event]),
+  )
+  const filteredMenuItems = menuItems.reduce<MenuItem[]>(
+    (activeMenu, menuItem) => {
+      const event = activeEventsMap.get(menuItem.value)
+      const updatedMenuItem = { ...menuItem }
+      if (event) {
+        updatedMenuItem.isNew = event.isNew
+        updatedMenuItem.isClaimedReward = event.isClaimedReward
+        activeMenu.push(updatedMenuItem)
+      } else if (menuItem.children && menuItem.children.length > 0) {
+        const activeChildren = generateMenuData(menuItem.children, activeEvents)
+        if (activeChildren.length) {
+          updatedMenuItem.children = activeChildren
+          activeMenu.push(updatedMenuItem)
+        }
+      }
+      return activeMenu
+    },
+    [],
+  )
+
+  // 对菜单进行排序，确保顺序与 activeEvents 一致
+  function sortMenuItemsByActiveEventsOrder(menuItems: MenuItem[]): MenuItem[] {
+    return menuItems.sort((a, b) => {
+      const indexA = activeEvents.findIndex(
+        (event) => event.activity === a.value,
+      )
+      const indexB = activeEvents.findIndex(
+        (event) => event.activity === b.value,
+      )
+      return indexA - indexB
+    })
+  }
+  const sortedMenuItems = sortMenuItemsByActiveEventsOrder(filteredMenuItems)
+  sortedMenuItems.forEach((menuItem) => {
+    if (menuItem.children && menuItem.children.length > 0) {
+      menuItem.children = sortMenuItemsByActiveEventsOrder(menuItem.children) // 对子菜单也应用排序
+    }
+  })
+
+  return sortedMenuItems
+}
+
+// 获取所有活动信息
+function getAllEvents(to: RouteLocationNormalized): void {
+  getPlayerMissionData({})
+    .then((res) => {
+      const menuStore = useMenuStore()
+      const { updateMenuData } = menuStore
+      const baseStore = useBaseStore()
+      const { updateBaseInfoItems } = baseStore
+      const activityStore = useActivityStore()
+      const { updateActivityTime } = activityStore
+
+      const activeEvents = extractActiveEvents(res.data.event_data)
+      const newMenuData = generateMenuData(initMenuItems, activeEvents)
+      console.log('newMenuData: ', newMenuData)
+      if (!newMenuData || newMenuData.length === 0) {
+        showToast('网络连接异常，请稍后重试')
+        return
+      }
+      // 跳转到第一个活动页面：
+      // - 进入首页 /
+      // - 活动未开启
+
+      if (
+        newMenuData.length > 0 &&
+        (to.path === '/' ||
+          !newMenuData.find((item) => item.routeName === to.name))
+      ) {
+        console.log('初始 router replace')
+        void router.replace({ name: newMenuData[0].routeName })
+      }
+
+      // 活动时间
+      const newActivityTime = activeEvents
+        .filter((item) => item.startTime && item.endTime)
+        .reduce<ActivityTime>((acc, cur) => {
+          acc[cur.activity] = formatDate(cur.startTime, cur.endTime)
+          return acc
+        }, {})
+
+      // 更新 store 存储
+      if (res.data?.current_time) {
+        updateBaseInfoItems({ currentTime: res.data.current_time })
+      }
+      updateActivityTime(newActivityTime)
+      updateMenuData(newMenuData)
+
+      // fix: 首次请求完菜单信息之后更新红点状态，路由拦截时数据还是初始数据
+      const module = to.meta.module
+
+      if (module && typeof module === 'string') {
+        menuStore.updatedMenuDataByRoute(module)
+      }
+    })
+    .catch((error) => {
+      console.log('router index', error)
+      showToast(error.message)
+    })
+}
+
+// 格式化日期
+function formatDate(startTime: number, endTime: number): string {
+  const start = dayjs.unix(startTime)
+  const end = dayjs.unix(endTime)
+  return `${start.format('M.D')}-${end.format('M.D')}`
+}
 
 export default router
