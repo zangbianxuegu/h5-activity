154c043f159496cdf1f38bea4aec00e7d28d44c0
chenzihao05 <wb.chenzihao05@mesg.corp.netease.com>
2024-05-27 20:30:41 +0800

    refs #51563 【活动中心】周年庆福利海报（前端页面）

---
+++ src/views/PosterAnniversary2024/PosterAnniversary2024.vue
@@ -0,0 +1,85 @@
+<template>
+  <Transition appear :name="bodyTransitionName" mode="out-in">
+    <div class="dragon-boat-2024 flex h-screen">
+      <div class="dragon-boat-2024-main"></div>
+    </div>
+  </Transition>
+</template>
+
+<script setup lang="ts">
+import { type DesignConfig } from '@/types'
+import useResponsiveStyles from '@/composables/useResponsiveStyles'
+
+// 设计稿宽
+const DESIGN_WIDTH = 2560
+// 设计稿高
+const DESIGN_HEIGHT = 1200
+// 设计稿主体宽，减去边距：因为我们要保留主体部分的边距。
+// 会影响最终计算出来的缩放系数，影响元素转换的实际大小，所以只能在这里减去，而不能在元素上写边距。
+const DESIGN_MAYDAY_WIDTH = 2100 - 60
+// 设计稿主体高，同宽。
+const DESIGN_MAYDAY_HEIGHT = 1200 - 60
+// 设计稿主体内容宽
+const DESIGN_MAYDAY_CONTENT_WIDTH = DESIGN_MAYDAY_WIDTH
+// 设计稿主体内容高
+const DESIGN_MAYDAY_CONTENT_HEIGHT = DESIGN_MAYDAY_HEIGHT
+// 设计稿主体内容宽高比
+const DESIGN_MAYDAY_CONTENT_RATIO =
+  DESIGN_MAYDAY_CONTENT_WIDTH / DESIGN_MAYDAY_CONTENT_HEIGHT
+
+// 配置参数
+const designConfig: DesignConfig = {
+  designWidth: DESIGN_WIDTH,
+  designHeight: DESIGN_HEIGHT,
+  designMainWidth: DESIGN_MAYDAY_WIDTH,
+  designMainHeight: DESIGN_MAYDAY_HEIGHT,
+  designMainContentWidth: DESIGN_MAYDAY_CONTENT_WIDTH,
+  designMainContentHeight: DESIGN_MAYDAY_CONTENT_HEIGHT,
+  designMainContentRatio: DESIGN_MAYDAY_CONTENT_RATIO,
+}
+
+// 缩放系数
+const { factor } = useResponsiveStyles(designConfig)
+console.log('factor: ', factor.value)
+
+const bodyTransitionName = ref('')
+bodyTransitionName.value = 'fade-in-body'
+</script>
+
+<style lang="scss" scoped>
+.fade-in-body-enter-active {
+  transition: opacity 1s ease-out;
+}
+.fade-in-body-enter-from {
+  opacity: 0.2;
+}
+.fade-in-head-enter-active {
+  transition: opacity 1s ease-out 0.2s;
+}
+.fade-in-head-enter-from {
+  opacity: 0.2;
+}
+.fade-in-main-enter-active {
+  transition: opacity 1s ease-out 0.5s;
+}
+.fade-in-main-enter-from {
+  opacity: 0.2;
+}
+.dragon-boat-2024 {
+  position: relative;
+  width: 2100px;
+
+  &-main {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%) scale(var(--scale-factor));
+    width: 2040px;
+    height: 1140px;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    background-image: url('@/assets/images/poster_anniversary_2024/bg.jpg');
+  }
+}
+</style>
