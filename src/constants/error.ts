bf9c810806d795001d710d872304d04188bb78a1
chenzihao05 <wb.chenzihao05@mesg.corp.netease.com>
2025-01-01 02:19:39 +0800

    fix: 绘梦节需求-添加投稿错误429提示语

--- src/constants/error.ts
+++ src/constants/error.ts
@@ -304,6 +304,9 @@ export const ERROR_MESSAGES: ErrorMessages = {
     500: {
       fail: '投稿失败',
     },
+    429: {
+      too_many_requests: '您的操作过于频繁，投稿间隔需要大于48小时',
+    },
     409: {
       'already exists': '作品信息已经存在，请刷新页面',
     },
