8e0851efd351e304974899bce5715ef49f33fd22
wb.dengeryi01 <wb.dengeryi01@mesg.corp.netease.com>
2025-01-02 17:31:12 +0800

    feat: 活动中心绘梦节取票站页面开发

--- src/views/Dayofdesign01Vote1Mission/Dayofdesign01Vote1Mission.vue
+++ src/views/Dayofdesign01Vote1Mission/Dayofdesign01Vote1Mission.vue
@@ -1,10 +1,12 @@
 <template>
   <Transition appear :name="bodyTransitionName" mode="out-in">
     <div class="page relative h-screen w-screen bg-cover bg-center">
-      <div class="h-full w-full">
+      <div
+        class="absolute bottom-0 left-0 right-0 top-0 m-auto flex h-full w-full flex-col items-center justify-center pl-[100px]"
+      >
         <Transition appear :name="headTransitionName" mode="out-in">
-          <header class="design-header relative">
-            <h1 class="title relative h-full">
+          <header class="relative ml-[480px] self-start">
+            <h1 class="title animate__animated animate__fadeIn relative h-full">
               <div class="sr-only">
                 取票站
                 <p>
@@ -20,25 +22,82 @@
           </header>
         </Transition>
         <Transition appear :name="mainTransitionName" mode="out-in">
-          <section>
+          <section
+            class="animate__animated animate__fadeIn flex flex-col items-center"
+          >
             <!-- 奖励 -->
-            <h2 class="sr-only">奖励</h2>
-            <div class="reward-container flex items-center justify-center">
-              <bubble
-                :reward="taskItem"
-                :bubbleScale="2.3"
-                :bounce-class="`${taskItem.taskId}`"
+            <div
+              class="task-container flex items-center justify-center gap-x-[120px]"
+            >
+              <div
+                v-for="(item, index) in taskList"
+                :key="index"
+                class="relative flex flex-col items-center"
               >
                 <div
                   :class="[
-                    'reward-item animate__animated animate__fadeIn',
-                    `${taskItem.status}`,
+                    'task-item animate__animated animate__fadeIn mb-[50px] mt-[40px]',
+                    `${item.status}`,
                   ]"
-                  @click="handleReward(1, taskItem)"
+                  @click="handleReward(1, item)"
                 ></div>
-              </bubble>
+                <div :class="['tray', `${item.status}`]"></div>
+                <div class="text-center text-[36px] text-white">
+                  {{ item.title }}（{{ item.val }}/{{ item.stages[0] }}）
+                </div>
+              </div>
             </div>
-            <div class="character"></div>
+            <!-- 累计奖励 -->
+            <div class="acc-task mt-[50px] flex w-[1480px] justify-between">
+              <div
+                class="mt-[144px] h-[48px] w-[229px] rounded-[24px] bg-[#f1f1f1] text-center text-[36px] text-[#5a7091] text-white"
+              >
+                累计投票
+              </div>
+              <div class="relative">
+                <!-- 累计奖励 -->
+                <ul class="flex w-[1150px] justify-between">
+                  <li
+                    v-for="(item, index) in accTaskList"
+                    :key="item.id"
+                    class="flex h-[230px] flex-col items-center justify-between"
+                  >
+                    <div
+                      :class="['acc-task-item', item.status]"
+                      @click="handleReward(index + 1, item)"
+                    ></div>
+                    <p
+                      class="h-[30px] text-center text-[30px] leading-[36px] text-white"
+                    >
+                      {{ item.stages[0] }}次
+                    </p>
+                  </li>
+                </ul>
+                <!-- 进度条 -->
+                <div
+                  class="progress-container absolute right-0 top-[162px] w-[1220px]"
+                >
+                  <div
+                    class="progress-bar"
+                    :style="`width: ${accTaskValue > 100 ? 100 : accTaskValue}%;`"
+                  ></div>
+                </div>
+              </div>
+            </div>
+            <!-- 按钮 -->
+            <button
+              type="button"
+              class="btn mt-[40px] rounded-full bg-[#ffee91] text-[#836a4e]"
+            >
+              <RouterLink
+                to="/dayofdesign01-post-submit"
+                class="block h-full w-full"
+              >
+                前往投票
+              </RouterLink>
+            </button>
+            <!-- 人物 -->
+            <div class="character animate__animated animate__fadeIn"></div>
           </section>
         </Transition>
         <!-- 活动规则弹框 -->
@@ -53,15 +112,15 @@ import { showToast } from 'vant'
 import { getPlayerMissionData, claimMissionReward } from '@/utils/request'
 import type { Event } from '@/types'
 import { useMenuStore } from '@/stores/menu'
-import { useActivityStore } from '@/stores/pcCelebration'
+import { useActivityStore } from '@/stores/dayofdesign01Vote1Mission'
 import { REWARD_MAP } from '@/constants/rewardMap'
 import ModalHelp from './components/ModalHelp.vue'
-import Bubble from '@/components/Bubble'
 import {
   EVENT_NAME,
   type TaskItem,
   type Reward,
-  createTaskItem,
+  createTaskItemLiist,
+  createBottomAccTaskList,
   SESSION_IS_VISITED_KEY,
 } from './config'
 import { useTransition } from '@/composables/useTransition'
@@ -69,7 +128,13 @@ import { useTransition } from '@/composables/useTransition'
 const { bodyTransitionName, headTransitionName, mainTransitionName } =
   useTransition(SESSION_IS_VISITED_KEY)
 
-const TASK_ITEM = createTaskItem()
+const TASK_LIST = createTaskItemLiist()
+const ACC_TASK_LIST = createBottomAccTaskList()
+
+// 任务排序
+const taskOrderMap = new Map(
+  [...TASK_LIST, ACC_TASK_LIST[0]].map((task, index) => [task.taskId, index]),
+)
 
 // 弹框
 const modalHelp = ref<InstanceType<typeof ModalHelp> | null>(null)
@@ -87,22 +152,44 @@ const curRewards: Ref<Reward[]> = ref([
   },
 ])
 
-// 获取任务状态
-const getTaskStatus = (award: number, value: number, stage: number): string => {
-  if (award === 1) return 'redeemed'
-  if (award === 0 && value >= stage) return 'can'
-  return 'wait'
-}
-
-// 任务列表
-const taskItem = computed(() => {
-  const activity = activityData.value.event_data[EVENT_NAME][0]
-  const { award, value, stages } = activity
-  return {
-    ...TASK_ITEM,
-    status: getTaskStatus(award[0], value, stages[0]),
-  }
+// 更新任务列表状态
+const taskList = computed(() => {
+  return TASK_LIST.map((item, index) => {
+    const { award, value, stages } = eventData.value[index]
+    return {
+      ...item,
+      val: value,
+      stages,
+      status: getTaskStatus(award[0], value, stages[0]),
+    }
+  })
 })
+
+// 更新累计任务列表状态
+const accTaskList = computed(() => {
+  return ACC_TASK_LIST.map((item, index) => {
+    const { award, value, stages } = eventData.value[3]
+    return {
+      ...item,
+      val: value,
+      stages,
+      status: getTaskStatus(award[index], value, stages[index]),
+    }
+  })
+})
+
+/**
+ * @function 获取任务状态
+ * @param award - 奖励状态，1表示已领取，0表示未领取
+ * @param value - 当前任务完成值
+ * @param stage - 任务目标值
+ * @returns {string} 任务状态
+ */
+const getTaskStatus = (award: number, value: number, stage: number): string => {
+  if (award === 1) return 'redeemed' // 如果奖励状态为1，表示已领取
+  if (award === 0 && value >= stage) return 'can' // 如果奖励状态为0且当前值大于等于目标值，表示可以领取
+  return 'wait' // 其他情况表示还在等待完成任务
+}
 
 onMounted(() => {
   try {
@@ -119,8 +206,21 @@ onMounted(() => {
 function getActivityData(): void {
   getPlayerMissionData({ event: EVENT_NAME })
     .then((res) => {
+      const data = res.data
+      const newActivityData = {
+        ...data,
+        event_data: {
+          [EVENT_NAME]: data.event_data[EVENT_NAME].sort(
+            (a: Event, b: Event) => {
+              const orderA = taskOrderMap.get(a.task_id) as number
+              const orderB = taskOrderMap.get(b.task_id) as number
+              return orderA - orderB
+            },
+          ),
+        },
+      }
       // 更新缓存活动数据
-      activityStore.updateActivityData(res.data)
+      activityStore.updateActivityData(newActivityData)
       setRedDot()
     })
     .catch((error) => {
@@ -149,9 +249,14 @@ function handleReward(rewardId: number, item: TaskItem): void {
     rewardId,
   })
     .then(async (res) => {
-      curRewards.value = res.data.rewards
+      const rewards = res.data.rewards
+      curRewards.value = rewards
       // 更新页面数据
-      activityData.value.event_data[EVENT_NAME][0].award[rewardId - 1] = 1
+      const taskIndex = eventData.value.findIndex(
+        (item) => item.task_id === taskId,
+      )
+      activityData.value.event_data[EVENT_NAME][taskIndex].award[rewardId - 1] =
+        1
       showToast(
         '领取成功，您获得了' +
           curRewards.value.map(
@@ -196,6 +301,31 @@ function setRedDot(): void {
   const hasUnclaimedReward = checkHasUnclaimedReward(eventData.value)
   menuStore.updateMenuDataByHasUnclaimedReward(EVENT_NAME, hasUnclaimedReward)
 }
+
+/**
+ * @function 适配累积值不等分的进度条，计算累积任务值
+ * @param accTaskVal 累积任务值
+ * @returns {number} 返回计算后的任务值
+ * @description 根据累积任务值计算进度条的值
+ */
+function calculateAccTaskValue(accTaskVal: number): number {
+  // 如果累积任务值大于等于50，直接返回100
+  if (accTaskVal >= 50) return 100
+  const ACC_TASK_VALUES: Readonly<Record<number, number>> = {
+    0: 0,
+    10: 12,
+    20: 32,
+    30: 53,
+    40: 74,
+    50: 100,
+  }
+  return ACC_TASK_VALUES[accTaskVal]
+}
+
+// 底部 进度：10,20,30,40,50
+const accTaskValue = computed(() => {
+  return calculateAccTaskValue(eventData.value[3].value)
+})
 </script>
 
 <style lang="scss" scoped>
@@ -222,9 +352,6 @@ function setRedDot(): void {
   background-image: url('@/assets/images/dayofdesign01/dayofdesign01-vote-mission/bg.jpg');
 }
 .title {
-  position: absolute;
-  left: 120px;
-  top: 130px;
   width: 685px;
   height: 163px;
   background-image: url('@/assets/images/dayofdesign01/dayofdesign01-vote-mission/title.png');
@@ -235,17 +362,30 @@ function setRedDot(): void {
   width: 83px;
   height: 84px;
   top: 54px;
-  right: -60px;
+  right: -56px;
   background-image: url('@/assets/images/dayofdesign01/dayofdesign01-vote-mission/help.png');
   background-repeat: no-repeat;
 }
-.reward-container {
-  position: absolute;
-  left: 600px;
-  top: 20%;
+.task-container {
   height: 458px;
   width: 1485px;
   background-image: url('@/assets/images/dayofdesign01/dayofdesign01-vote-mission/top-reward-bg.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+.task-container::after {
+  content: '投票次数获取后仅当日有效';
+  font-size: 34px;
+  padding-top: 50px;
+  padding-left: 30px;
+  color: #5a7091;
+  position: absolute;
+  width: 476px;
+  height: 134px;
+  top: -130px;
+  right: 0;
+  background-image: url('@/assets/images/dayofdesign01/dayofdesign01-vote-mission/bubble-bg.png');
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
@@ -261,12 +401,14 @@ function setRedDot(): void {
   background-size: cover;
   background-position: center;
 }
-.reward-item {
+.task-item {
   width: 208px;
   height: 208px;
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
+  position: relative;
+  z-index: 10;
   &.wait {
     background-image: url('@/assets/images/dayofdesign01/dayofdesign01-vote-mission/task-wait.png');
   }
@@ -275,8 +417,81 @@ function setRedDot(): void {
     background-image: url('@/assets/images/dayofdesign01/dayofdesign01-vote-mission/task-can.png');
   }
   &.redeemed {
+    width: 208px;
+    height: 217px;
     transition: background-image 1s ease;
     background-image: url('@/assets/images/dayofdesign01/dayofdesign01-vote-mission/task-redeemed.png');
   }
+}
+.tray {
+  position: absolute;
+  width: 257px;
+  height: 134px;
+  top: 156px;
+  left: 0;
+  right: 0;
+  margin: auto;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  &.wait {
+    background-image: url('@/assets/images/dayofdesign01/dayofdesign01-vote-mission/tray-wait.png');
+  }
+  &.can {
+    background-image: url('@/assets/images/dayofdesign01/dayofdesign01-vote-mission/tray-can.png');
+  }
+  &.redeemed {
+    background-image: url('@/assets/images/dayofdesign01/dayofdesign01-vote-mission/tray-redeemed.png');
+  }
+}
+.acc-task-item {
+  width: 142px;
+  height: 142px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  &.wait {
+    background-image: url('@/assets/images/dayofdesign01/dayofdesign01-vote-mission/acc-task-wait.png');
+  }
+  &.can {
+    background-image: url('@/assets/images/dayofdesign01/dayofdesign01-vote-mission/acc-task-can.png');
+  }
+  &.redeemed {
+    transition: background-image 1s ease;
+    background-image: url('@/assets/images/dayofdesign01/dayofdesign01-vote-mission/acc-task-redeemed.png');
+  }
+}
+.progress-container {
+  height: 20px;
+  background-color: rgba($color: #fff, $alpha: 0.3);
+  border-radius: 8px;
+}
+.progress-bar {
+  height: 20px;
+  background-color: #ffd871;
+  border-radius: 8px;
+  transition: width 0.4s ease;
+  position: relative;
+}
+.progress-bar::after {
+  content: '';
+  position: absolute;
+  right: -20px;
+  top: -14px;
+  height: 46px;
+  width: 40px;
+  background-image: url('@/assets/images/dayofdesign01/dayofdesign01-vote-mission/star.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  transition: right 0.4s ease;
+}
+.btn {
+  font-size: 40px;
+  color: #836a4e;
+  width: 430px;
+  height: 87px;
+  line-height: 87px;
+  text-align: center;
+  box-shadow: 0 6px 6px 0 rgba(81, 81, 81, 0.2);
 }
 </style>
--- src/views/Dayofdesign01Vote1Mission/config.ts
+++ src/views/Dayofdesign01Vote1Mission/config.ts
@@ -1,8 +1,6 @@
-import type CanRewardBubbleAnimation from '@/components/CanRewardBubbleAnimation'
-
 // 常量
-export const EVENT_NAME = 'activitycenter_pc_celebration'
-export const SESSION_IS_VISITED_KEY = 'isVisited Dayofdesign01VoteMission'
+export const EVENT_NAME = 'activitycenter_dayofdesign01_vote1_mission'
+export const SESSION_IS_VISITED_KEY = 'isVisitedDayofdesign01VoteMission'
 
 // 奖励类型
 export interface Reward {
@@ -16,20 +14,82 @@ export interface TaskItem {
   taskId: string
   title: string
   status: 'wait' | 'redeemed' | 'can' | string
-  canRewardLottieRef: Ref<Array<InstanceType<typeof CanRewardBubbleAnimation>>>
-  hadRenderLottie?: Ref<boolean>
+  val: number
+  stages: number[]
 }
 
 // 任务列表
-export function createTaskItem(): TaskItem {
-  return {
-    id: 1,
-    taskId: 'activitycenter_pc_celebration_m1',
-    title: '领光标发饰',
-    status: 'wait',
-    canRewardLottieRef: ref() as Ref<
-      Array<InstanceType<typeof CanRewardBubbleAnimation>>
-    >,
-    hadRenderLottie: ref(false),
-  }
+export function createTaskItemLiist(): TaskItem[] {
+  return [
+    {
+      id: 1,
+      taskId: 'activitycenter_dayofdesign01_vote1_mission_m1',
+      title: '登录游戏',
+      status: 'wait',
+      val: 0,
+      stages: [1],
+    },
+    {
+      id: 2,
+      taskId: 'activitycenter_dayofdesign01_vote1_mission_m2',
+      title: '查看作品详情',
+      status: 'wait',
+      val: 0,
+      stages: [1],
+    },
+    {
+      id: 3,
+      taskId: 'activitycenter_dayofdesign01_vote1_mission_m3',
+      title: '分享作品',
+      status: 'wait',
+      val: 0,
+      stages: [1],
+    },
+  ]
+}
+
+// 被邀请玩家累计献祭次数
+export function createBottomAccTaskList(): TaskItem[] {
+  return [
+    {
+      id: 1,
+      taskId: 'activitycenter_dayofdesign01_vote1_mission_m4',
+      title: '累计投票10次',
+      status: 'wait',
+      val: 0,
+      stages: [10],
+    },
+    {
+      id: 2,
+      taskId: 'activitycenter_dayofdesign01_vote1_mission_m4',
+      title: '累计投票20次',
+      status: 'wait',
+      val: 0,
+      stages: [20],
+    },
+    {
+      id: 3,
+      taskId: 'activitycenter_dayofdesign01_vote1_mission_m4',
+      title: '累计投票30次',
+      status: 'wait',
+      val: 0,
+      stages: [30],
+    },
+    {
+      id: 4,
+      taskId: 'activitycenter_dayofdesign01_vote1_mission_m4',
+      title: '累计投票40次',
+      status: 'wait',
+      val: 0,
+      stages: [40],
+    },
+    {
+      id: 5,
+      taskId: 'activitycenter_invitation_code_m4',
+      title: '累计投票50次',
+      status: 'wait',
+      val: 0,
+      stages: [50],
+    },
+  ]
 }
